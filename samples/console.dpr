9900607bff1df25a76f5c3b19fc09ed2e2eaef35
PassByYou888 <197090@qq.com>
2018-04-12 09:58:00 +0800

    update readme.md

--- samples/console.dpr
+++ samples/console.dpr
@@ -150,7 +150,7 @@ begin
 
   // 从stream快速读取op，这便于我们在
   m64.Position := 0;
-  if LoadOpFromStream(True, m64, op) then
+  if LoadOpFromStream(m64, op) then
     begin
       DoStatus('op运行返回值(正确值为4489.2962): %s', [VarToStr(op.Execute(rt))]);
     end;
